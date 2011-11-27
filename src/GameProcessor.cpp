776a24fc19d81beac6d51d89286f025f290d2cd8
florel_t <thomas.florelli@epitech.eu>
2011-11-27 06:05:28 +0100

    lol

--- src/GameProcessor.cpp
+++ src/GameProcessor.cpp
@@ -57,7 +57,6 @@ void GameProcessor::start(void) {
     QTimer *ennemy1Timer = new QTimer();
     ennemy1Timer->connect(ennemy1Timer, SIGNAL(timeout()), this, SLOT(spawnEnnemy1()));
     ennemy1Timer->start(1000/3);
-
 }
 
 void GameProcessor::generateEntity(const char *name) {
