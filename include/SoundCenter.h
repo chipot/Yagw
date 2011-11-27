cbcfacbdee144e1321af7d22c43327b756e180f5
PICHOT `Chipot\' Fabien <pichot.fabien@gmail.com>
2011-11-27 19:11:19 +0900

    SoundCenterv2.2
    Ça marche ou quoi ??

--- include/SoundCenter.h
+++ include/SoundCenter.h
@@ -2,37 +2,63 @@
 # define SOUNDCENTER_H_
 
 #include <QString>
-#include <QMap>
+#include <QMultiMap>
 #include <QList>
 #include <QObject>
 #include <QSignalMapper>
 #include "singleton.hpp"
+#include <Phonon>
+#include <Phonon/MediaSource>
 
-// forward declarations
-namespace Phonon {
-class MediaObject;
-class MediaSource;
-}
-class QSound;
-// end of forward declarations
+class SoundPair : public QObject
+{
+  Q_OBJECT
+ public:
+  SoundPair(Phonon::MediaSource const &ms) : QObject(), _mo(0), 
+    _ao(0), source(ms)
+  {
+    _mo = new Phonon::MediaObject();
+    _ao = new Phonon::AudioOutput(Phonon::GameCategory);
+    Phonon::createPath(_mo, _ao);
+    _mo->setCurrentSource(source);
+    QObject::connect(_mo, SIGNAL(finished()), this, SLOT(loop_prepare()));
+  }
+
+  void play()
+  {
+    _mo->play();
+  }
+  Phonon::State state()
+  {
+    return _mo->state();
+  }
+ private slots:
+  void loop_prepare()
+  {
+    _mo->clear();
+    _mo->enqueue(source);
+  }
+ private:
+  Phonon::MediaObject* _mo;
+  Phonon::AudioOutput* _ao;
+  Phonon::MediaSource source;
+};
 
 class SoundCenter: public QObject, public moost::singleton<SoundCenter>
 {
     Q_OBJECT
     friend class moost::singleton<SoundCenter>;
-    typedef QMap<QString, Phonon::MediaSource*> SoundRelationMap;
+    typedef QMultiMap<QString, SoundPair*> SoundRelationMap;
     static const int MAX_SOUNDS = 5;
   public:
     void play(const QString &name);
   public slots:
     void loop();
-    void song_finished(QObject *);
   private:
     SoundCenter();
+    ~SoundCenter();
     SoundRelationMap data;
     Phonon::MediaObject *music;
-    QSignalMapper signal_map;
-    int current_sounds;
 };
 
 #endif /* !SOUNDCENTER_H_ */
--- src/SoundCenter.cpp
+++ src/SoundCenter.cpp
@@ -1,21 +1,7 @@
-#include <Phonon>
-#include <Phonon/MediaSource>
-#include <QSound>
 #include <QDebug>
 #include <QPair>
 #include <QFileInfo>
 #include "SoundCenter.h"
-
-namespace {
-template <typename T, typename U>
-class MyPair : public QObject, public QPair<T, U>
-{
- public:
-  MyPair(T const &t, U const &u) :QObject(), QPair<T,U>(t, u)
-  {
-  }
-};
-}
 
 SoundCenter::SoundCenter()
   :data(),
@@ -23,55 +9,47 @@ SoundCenter::SoundCenter()
             Phonon::MusicCategory,
             Phonon::MediaSource("ressource/background_music.mp3")
             )
-         ),
-    signal_map(),
-    current_sounds(0)
+         )
 {
   this->music->play();
-  current_sounds++;
-  data["shoot"] = new Phonon::MediaSource("ressource/shoot.wav");
-  data["kill"] = new Phonon::MediaSource("./ressource/kill.wav");
+  data.insert("shoot", new SoundPair(Phonon::MediaSource("./ressource/shoot.wav")));
+  data.insert("shoot", new SoundPair(Phonon::MediaSource("./ressource/shoot.wav")));
+  data.insert("shoot", new SoundPair(Phonon::MediaSource("./ressource/shoot.wav")));
+  data.insert("shoot", new SoundPair(Phonon::MediaSource("./ressource/shoot.wav")));
+  data.insert("shoot", new SoundPair(Phonon::MediaSource("./ressource/shoot.wav")));
+  data.insert("kill", new SoundPair(Phonon::MediaSource("./ressource/kill.wav")));
+  data.insert("kill", new SoundPair(Phonon::MediaSource("./ressource/kill.wav")));
+  data.insert("kill", new SoundPair(Phonon::MediaSource("./ressource/kill.wav")));
   connect(this->music, SIGNAL(aboutToFinish()), this, SLOT(loop()));
-  connect(&signal_map, SIGNAL(mapped(QObject *)),
-          this, SLOT(song_finished(QObject*)));
+}
+
+SoundCenter::~SoundCenter()
+{
+  SoundRelationMap::mapped_type media_ptr;
+  foreach(media_ptr, data)
+  {
+    delete media_ptr;
+  }
+  data.clear();
 }
 
 void SoundCenter::play(const QString &name)
 {
-  return;
-  SoundRelationMap::const_iterator it = data.constFind(name);
-  if (it != this->data.constEnd() && current_sounds < MAX_SOUNDS)
+  QList<SoundRelationMap::mapped_type> l = data.values(name);
+  SoundRelationMap::mapped_type v;
+  foreach (v, l)
   {
-    MyPair<Phonon::MediaObject*,Phonon::AudioOutput*> *pair =
-        new MyPair<Phonon::MediaObject*,Phonon::AudioOutput*>(
-            new Phonon::MediaObject(),
-            new Phonon::AudioOutput(Phonon::GameCategory));
-
-    Phonon::createPath(pair->first, pair->second);
-    pair->first->setCurrentSource(**it);
-
-    connect(pair->first, SIGNAL(finished()), &signal_map, SLOT(map()));
-    signal_map.setMapping(pair->first, static_cast<QObject*>(pair));
-
-    pair->first->play();
-    current_sounds++;
-    qDebug() << "[SoundCenter]" << "Play" << name;
+    if (v->state() == Phonon::PlayingState)
+      continue;
+    else
+    {
+      v->play();
+      break;
+    }
   }
 }
 
 void    SoundCenter::loop()
 {
   this->music->enqueue(Phonon::MediaSource("./ressource/background_music.wav"));
-}
-
-void    SoundCenter::song_finished(QObject *obj)
-{
-  MyPair<Phonon::MediaObject*,Phonon::AudioOutput*>  *pair =
-      static_cast<MyPair<Phonon::MediaObject*,Phonon::AudioOutput*>*>(obj);
-  pair->first->clear();
-  pair->first->disconnect(SIGNAL(finished()), &signal_map, SLOT(map()));
-  delete pair->first;
-  delete pair->second;
-  delete pair;
-  current_sounds--;
 }
