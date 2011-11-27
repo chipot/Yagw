fdd1d5d08ab62ff53f48e74c4deae69d1fe37a49
PICHOT `Chipot\' Fabien <pichot.fabien@gmail.com>
2011-11-27 15:43:19 +0900

    Modifications de Soundcenter pour passer en Phonon only
    Soundcenter.h: Passage en forward declaration de toutes les classes dont
    on a pas besoin dans le .h, declaration d'un typedef pour la map de
    relation nom<=>son, ce qui permet de moins se faire chier quand on edit
    sont type. Rajout d'un deuxième MediaObject "sfx" qui va servire à lire
    les sons dedié aux sons de tir et d'explosion. Le MediaObject music
    sert uniquement à jouer la musique de fond. IL FAUT TESTER ÇA SUR
    D'AUTRES OS !!!
    SoundCenter.cpp: Correction d'un faille dans la methode play, si le nom
    n'existe pas, ça segfaultait, et selon Antoine "C'est normal LOL".
    Desormais, lorsque qu'on play un sound, on change la cible du Sfx, et on
    play le nouveau son (opti?).

--- src/SoundCenter.cpp
+++ src/SoundCenter.cpp
@@ -1,21 +1,38 @@
+#include <Phonon>
 #include <Phonon/MediaSource>
-#include "SoundCenter.h"
+#include <QSound>
 #include <QDebug>
+#include <QFileInfo>
+#include "SoundCenter.h"
 
 SoundCenter::SoundCenter()
-  :data(),   music(Phonon::createPlayer(Phonon::MusicCategory,
-                                        Phonon::MediaSource("ressource/background_music.wav")))
+  :data(),
+  music(Phonon::createPlayer(
+          Phonon::MusicCategory,
+          Phonon::MediaSource("ressource/background_music.mp3")
+          )
+      ),
+   sfx(Phonon::createPlayer(Phonon::GameCategory))
+   
 {
   this->music->play();
-  data["shoot"] = new QSound("ressource/shoot.wav");
-  data["quack"] = new QSound("ressource/quack.wav");
-  data["kill"] = new QSound("ressource/kill.wav");
+  //data["shoot"] = new QSound("./ressource/shoot.wav");
+  //data["quack"] = new QSound("./ressource/quack.wav");
+  //data["kill"] = new QSound("./ressource/kill.wav");
+  data["shoot"] = new Phonon::MediaSource("ressource/shoot.wav");
+  data["kill"] = new Phonon::MediaSource("./ressource/kill.wav");
   connect(this->music, SIGNAL(aboutToFinish()), this, SLOT(loop()));
 }
 
 void SoundCenter::play(const QString &name)
 {
-  data[name]->play();
+  SoundRelationMap::const_iterator it = data.constFind(name);
+  if (it != this->data.constEnd())
+  {
+    sfx->setCurrentSource(**it);
+    sfx->play();
+    qDebug() << "[SoundCenter]" << "Play" << name;
+  }
 }
 
 void    SoundCenter::loop()
